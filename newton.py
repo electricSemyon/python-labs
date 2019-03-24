9418a1786560b0ec110fa8e15c3394d137377c19
Slava <electricsemyon@gmail.com>
2019-03-24 15:56:16 +0200

    Update newton.py

--- newton.py
+++ newton.py
@@ -2,15 +2,18 @@ from math import cos, sin
 import unittest
 
 def dx(f, x):
-    return abs(0-f(x))
+    return abs(0 - f(x))
 
 def newton(f, df, x0, e):
-    delta = dx(f, x0)
-    while delta > e:
-        x0 = x0 - f(x0) / df(x0)
-        delta = dx(f, x0)
+  iterations_count = 0
+  delta = dx(f, x0)
 
-    return (x0, f(x0))
+  while delta > e:
+    x0 = x0 - f(x0) / df(x0)
+    delta = dx(f, x0)
+    iterations_count += 1
+
+  return (x0, f(x0), iterations_count)
 
 class NewtonTest(unittest.TestCase):
   def test_root_find(self):
@@ -21,7 +24,7 @@ class NewtonTest(unittest.TestCase):
       return x ** 2 * cos(2 * x) + 1
       
     actual_result = newton(f, df, 2, 0.0001)
-    expected_result = (1.1832211213551906, -5.082387317756343e-05)
+    expected_result = (1.1832211213551906, -5.082387317756343e-05, 12)
 
     self.assertEqual(actual_result, expected_result)
 
